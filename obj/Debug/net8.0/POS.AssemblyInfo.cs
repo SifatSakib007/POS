a2eea80d697e900c1cce2ce019a21010886418ea
ramohin1236 <mohinr26@gmail.com>
2024-10-09 12:00:34 -0700

    git problem solution

--- obj/Debug/net8.0/POS.AssemblyInfo.cs
+++ obj/Debug/net8.0/POS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7505bd47db3c9fc765561f5efde47d101b3ff429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d15c9f20791bcb8932ad9d8ff21f688bb0fc10")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
